b827490212b5fbdeb992ea3e40dcd8be9ca69925
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-13 19:55:10 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import cmd
+class Shell(cmd.Cmd):
+	intro = "Welcome to my customized interactive shell"
+	
+	def do_EOF(self, line):
+		print()
+		return True
+
+if __name__ == '__main__':
+	Shell().cmdloop()
