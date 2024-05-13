9c3a2222f39cc018505e6250986baaa9dfb30d1e
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-13 21:23:09 +0300

    Created tests/ and models/

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import cmd
 
 
 class Shell(cmd.Cmd):
+    """nljaskldasj
 
+    Args:
+        cmd (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     intro = "Welcome to my customized interactive shell"
 
     def do_EOF(self, line):
         print()
-
         return True
 
 
 if __name__ == '__main__':
+    print(Shell().do_EOF.__doc__)
     Shell().cmdloop()
