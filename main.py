03ed90928a5a2d020cc52a28e83bf549af8b6cf8
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-13 20:29:48 +0300

    edited main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import cmd
+
+
 class Shell(cmd.Cmd):
-	intro = "Welcome to my customized interactive shell"
-	
-	def do_EOF(self, line):
-		print()
-		return True
+
+    intro = "Welcome to my customized interactive shell"
+
+    def do_EOF(self, line):
+        print()
+
+        return True
+
 
 if __name__ == '__main__':
-	Shell().cmdloop()
+    Shell().cmdloop()
