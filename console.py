74c6fb2827b0a1e073315b3e70114b72835d8d20
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-14 21:56:49 +0300

    Added console.py and modified the behaviour of emptyline

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def emptyline(self):
+        pass
+
+    def do_quit(self, line):
+        '''Quit command to exit the program\n'''
+        return True
+
+    def do_EOF(self, line):
+        '''Terminates the console'''
+        return True
+
+
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
