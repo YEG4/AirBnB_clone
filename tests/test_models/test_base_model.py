860f617bb3d0ec63c9229998577154f011f8b834
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-19 19:16:48 +0300

    added test files in tests/

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,14 @@
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_add(self):
+        self.assertEqual(10, 10)
+
+
+if __name__ == "__main__":
+    unittest.main()
