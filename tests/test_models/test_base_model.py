dc94e046654008830386f28045521a0e297918ab
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-19 19:39:11 +0300

    Added docstrings to tests/test_models/test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,10 +5,16 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
+    """This class is used to test BaseModel class
+    """
     def setUp(self):
+        """setUp method that runs before every test
+        """
         pass
 
     def test_add(self):
+        """A TestComment
+        """
         self.assertEqual(10, 10)
 
 
