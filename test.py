8b4a4f6f26e60b06a153ec6b5351312cbba35ace
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-19 19:36:29 +0300

    Added docstrings to console.py

--- test.py
+++ test.py
@@ -1,30 +1,33 @@
-from models.base_model import BaseModel
-import os, json
-file_path = "file.json"
-classes = {
-    "BaseModel": "dhasjdhjas",
-    "dashdkas": 312312,
-    "jdkladjklsa": {"hdjad":"gdjagdas"}
-}
+# from models.base_model import BaseModel
+# import os, json
+# file_path = "file.json"
+# classes = {
+#     "BaseModel": "dhasjdhjas",
+#     "dashdkas": 312312,
+#     "jdkladjklsa": {"hdjad":"gdjagdas"}
+# }
 
-if os.path.isfile(file_path) and file_path.endswith(".json"):
-            with open(file_path, "r") as file:
-                instances = json.load(file)
+# if os.path.isfile(file_path) and file_path.endswith(".json"):
+#             with open(file_path, "r") as file:
+#                 instances = json.load(file)
 
 
 
-def func(cls,  id:str):
-    print(str(id))
-    joined_string = f"{cls}.{id}"
-    print(joined_string)
-my_list = []
-my_list.append(classes)
-my_list.append("Amazing")
+# def func(cls,  id:str):
+#     print(str(id))
+#     joined_string = f"{cls}.{id}"
+#     print(joined_string)
+# my_list = []
+# my_list.append(classes)
+# my_list.append("Amazing")
 
-if ['']:
-       print("True")
-else: 
-      print("False")
+# if ['']:
+#        print("True")
+# else: 
+#       print("False")
+
+import console
+
 
 
 
