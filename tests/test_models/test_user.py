47ece105c41e9ac11160b4771813e6f2acef3a14
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-19 21:11:44 +0300

    Added docstrings to tests/test_models/test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,7 +1,14 @@
+"""This test file is used to test User class
+"""
 import unittest
 
 
 class TestUser(unittest.TestCase):
+    """This is a TestUser class to test User class
+
+    Args:
+        unittest (module): a module used to do unittests
+    """
     pass
 
 
